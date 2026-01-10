046b4f923d6966a892b583f6f827171a07333b1b
ErenYeager-cse <karanbnsl2006@gmail.com>
2026-01-11 02:51:12 +0530

    dsa practice

---
+++ java dsa/src/DSA/Sorting/Cyclic_Sort/Cyclic_Sort.java
@@ -0,0 +1,29 @@
+package DSA.Sorting.Cyclic_Sort;
+
+import java.util.Arrays;
+
+public class Cyclic_Sort {
+    public static void main(String[] args) {
+        int[] arr = {2,5,4,3,1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr){
+        int i = 0;
+        while (i < arr.length){
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]){        // I think (i != correct) will also work
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first , int second){
+        int temp = arr[first];
+        arr[first]= arr[second];
+        arr[second]= temp;
+    }
+}
